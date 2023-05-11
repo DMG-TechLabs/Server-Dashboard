b25ef7f9ebcf838562757a4bfc27d9686fb0c730
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-05-11 21:15:11 +0300

    blue css file added

--- docs/script.js
+++ docs/script.js
@@ -1,26 +1,26 @@
 const container = document.getElementById("container");
 
-window.onchange = e => {changeAspectRatio();};
+window.onchange = e => { changeAspectRatio(); };
 
 function calculateAspectRatio(columns) {
     let num = projects.length;
-  
+
     var rows = 1;
     if (num > 6) {
-      rows = num / columns;
+        rows = num / columns;
     }
-  
+
     aspectRatio = columns.toString().concat(" / ".concat(rows));
-  
+
     container.style.aspectRatio = aspectRatio;
-  }
-  
-  function changeAspectRatio(){
-      if (window.innerWidth <= 600) {
-          calculateAspectRatio(3);
-        } else if (window.innerWidth > 600 && window.innerWidth <= 800){
-          calculateAspectRatio(4);
-        } else {
-          calculateAspectRatio(6);
-        }
-  }
+}
+
+function changeAspectRatio() {
+    if (window.innerWidth <= 600) {
+        calculateAspectRatio(3);
+    } else if (window.innerWidth > 600 && window.innerWidth <= 800) {
+        calculateAspectRatio(4);
+    } else {
+        calculateAspectRatio(6);
+    }
+}
